389acc42ff012bbfe427833c8e31efec4d6c1e98
user <ar.grig7@gmail.com>
2021-09-18 12:54:07 +0400

    size input added, output formattted, ** replaced with much faster operation

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from random import choice
 from copy import deepcopy
 
+print("Input matrix size:")
+size=int(input())
 def det(arr):
     size_arr = len(arr)
     if size_arr == 1: return arr[0][0]
@@ -10,12 +12,10 @@ def det(arr):
         del (arr_deepcopy[0])
         for i in range(len(arr_deepcopy)):
             del (arr_deepcopy[i][index_x])
-        result += arr[0][index_x] * (-1)**index_x * det(arr_deepcopy)
+        result += arr[0][index_x] * (-1 if index_x & 1 else 1 ) * det(arr_deepcopy)
     return result
 
-size = 5
+a = [[choice(range(1, 100)) for i in range(size)] for j in range(size)]
 
-a = [[choice(range(1, 10)) for i in range(size)] for j in range(size)]
-
-print(a)
+print("\n".join([" ".join([str(item) for item in row]) for row in a]))
 print(det(a))
