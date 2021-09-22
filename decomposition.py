38d21ad08152263faaf575e724918289dd90e98b
Yura Movsesyan <56353539+yuraMovsesyan@users.noreply.github.com>
2021-09-22 23:56:44 +0300

    add decomposition

---
+++ decomposition.py
@@ -0,0 +1,30 @@
+from random import choice
+from copy import deepcopy
+import time
+
+size = int(input("Input matrix size: "))
+
+min_choice = 1
+max_choice = 100
+
+def det(arr):
+    size_arr = len(arr)
+    if size_arr == 1: return arr[0][0]
+    result = 0
+    for index_x in range(size_arr):
+        arr_deepcopy = deepcopy(arr)
+        del (arr_deepcopy[0])
+        for i in range(len(arr_deepcopy)):
+            del (arr_deepcopy[i][index_x])
+        result += arr[0][index_x] * (-1 if index_x & 1 else 1 ) * det(arr_deepcopy)
+    return result
+
+a = [[choice(range(min_choice, max_choice)) for i in range(size)] for j in range(size)]
+
+for y in a:
+    for x in y:
+        print(str(x) + " " * (len(str(max_choice)) - len(str(x))), end='')
+    print("")
+start_time = time.time()
+print("Determinant:", det(a))
+print("--- %s seconds ---" % (time.time() - start_time))
