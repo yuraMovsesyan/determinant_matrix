ef5fc8b60f329387ff3822f49759b52c5a4a3965
Yura Movsesyan <56353539+yuraMovsesyan@users.noreply.github.com>
2021-09-22 20:23:17 +0300

    added function getPermutations

--- main.py
+++ main.py
@@ -7,6 +7,24 @@ size = int(input("Input matrix size: "))
 min_choice = 1
 max_choice = 10
 
+def getPermutations(arr, flag = False):
+    if not flag:
+        arr = list(arr)
+
+    if len(arr) == 1:
+        return [arr]
+
+    iterate_perm = getPermutations(arr[1:], True)
+
+    result_perm = []
+    number = [arr[0]]
+
+    for perm in iterate_perm:
+        for i in range(0, len(perm) + 1):
+            result_perm.append(perm[:i] + number + perm[i:])
+    return result_perm
+
+
 def getInversion(arr):
     size_arr = len(arr)
     result_count = 0
@@ -23,7 +41,7 @@ def det(arr):
     if size_arr == 1: return arr[0][0]
     result = 0
 
-    for item in itertools.permutations(range(0, size_arr)):
+    for item in getPermutations(range(0, size_arr)):
         res = 1
         for id in range(size_arr):
             res *= arr[id][item[id]]
