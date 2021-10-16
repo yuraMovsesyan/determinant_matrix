3c3a6745b1f9a3b684518ded986c60b2bfe75caf
Yura Movsesyan <56353539+yuraMovsesyan@users.noreply.github.com>
2021-10-16 22:48:42 +0300

    set main

--- Rust/src/main.rs
+++ Rust/src/main.rs
@@ -10,15 +10,6 @@ fn main() {
     let rank = matrix::rank(matrix.clone());
     matrix::print(&mut matrix);
 
-    //matrix::print(&mut rank);
     println!("rank: {}", rank);
-
-    let le = matrix[1].len();
-
-    let v:Vec<f64> = matrix::get_column(&mut matrix, le - 1);
-
-    println!("line: {:?}",v)
-
-    //matrix::inv(matrix.clone());
 
 }
